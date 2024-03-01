ddcfe81f99837d4dd9df347f164109923c96b9ff
christophermccall <camccall24@gmail.com>
2024-03-01 00:58:07 -0500

    made class, added getters and setters.

--- src/main/java/com/github/zipcodewilmington/Hangman.java
+++ src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -1,5 +1,10 @@
 package com.github.zipcodewilmington;
 
+import java.util.Random;
+
+/*find out hot to Generate a random word
+- use array inside of class?
+*/
 /**
  * @author xt0fer
  * @version 1.0.0
@@ -8,4 +13,45 @@ package com.github.zipcodewilmington;
 
 public class Hangman {
     //Chris McCall
+    private String grape;
+    private String orange;
+    private String toothpaste;
+
+    public Hangman (String grape, String orange, String toothpaste){
+
+        this.grape = grape;
+        this.orange = orange;
+        this.toothpaste = toothpaste;
+
+
+
+    }
+
+        public String getGrape(){
+        return grape;
+        }
+
+        private void setGrape(String grape){
+        this.grape = grape;
+        }
+
+        public String getOrange(){
+        return orange;
+        }
+
+        private void setOrange(String orange){
+        this.orange = orange;
+        }
+
+        public String getToothpaste(){
+        return toothpaste;
+        };
+        private void setToothpaste(String toothpaste){
+            this.toothpaste = toothpaste;
+        }
+
+
+
+
+
 }
---
+++ src/main/java/com/github/zipcodewilmington/main.java
@@ -0,0 +1,8 @@
+package com.github.zipcodewilmington;
+
+public class main {
+
+    public static void main(String[] args){
+
+    }
+}
